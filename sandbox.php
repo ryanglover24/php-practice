21b10340f91d77b1400adb78516b600396d5baa9
ryang <r.gluger23@gmail.com>
2019-10-25 00:05:34 +0100

    practice ternary operators

---
+++ sandbox.php
@@ -0,0 +1,29 @@
+<?php 
+//ternary operators
+
+$score = 50;
+
+// $val = $score > 40 ? 'high score!' : 'low score :(';
+// echo $val;
+
+// echo $score > 40 ? 'high score!' : 'low score :(';
+
+
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title>php tuts</title>
+    </head>
+    <body>
+        
+        
+        <p><?php echo $score > 40 ? 'high score!' : 'low score :('; ?></p>
+
+
+    </body>
+</html>
