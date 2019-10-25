9d28f63ea613c11e67bd6c4fbf495be9d00b4353
ryang <r.gluger23@gmail.com>
2019-10-25 01:12:19 +0100

    l-40 file system p-2

--- sandbox.php
+++ sandbox.php
@@ -1,35 +1,31 @@
 <?php 
 
- //file system - part 1
-//reading a txt file (number at end of content is number of bytes in the file)
-//  $quotes = readfile('readme.txt');
-//  echo $quotes;
+ //file system - part 2
 
-//checking file exists
-    $file = 'readme.txt';
+    $file = 'quotes.txt';
 
-    if(file_exists($file)) {
-        //readfile
-        echo readfile($file) . '<br />';
+    // opening a file for reading second param is code in this case r for read only
+    $handle = fopen($file, 'a+');
 
-        //copyfile doesnt matter doesnt exist will make for us
-        copy($file, 'quotes.txt');
+    //read the file second param is how many bytes want to read
+    // echo fread($handle, filesize($file)) . '<br />';
 
-        //absolute path
-        echo realpath($file) . '<br />';
+    //read a single line second instance reads second line 'pointer' follows on next example would also follow and not go to the start
+    // echo fgets($handle) . '<br />';
+    // echo fgets($handle) . '<br />';
 
-        //file size
-        echo filesize($file) . '<br />';
+    //read a single character
+    // echo fgetc($handle);
+    // echo fgetc($handle);
 
-        // rename file
-        rename($file, 'test.txt');
+    //writing to the file code becomes r+ read and write. Writes at start and writes over text a+ puts pointer at end and writes to end
+    fwrite($handle, "\nEverything Popular is wrong");
 
-    } else {
-        echo 'file does not exist';
-    }
+    //close the file when finised
+    fclose($handle);
 
-    //make directory
-    mkdir('quotes');
+    //delete a file 
+    unlink($file);
 
 ?>
 
